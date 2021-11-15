24ec38aea21bade4c516389680ae2777e15d1364
Kevin Bryan Buenaseda <kevinbryanbuenaseda@gmail.com>
2021-11-16 00:58:03 +0800

    12:57am 16/11/21

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var path = require("path");
 var express = require("express");
 var app = express();
-
 app.use(express.static(__dirname + "/"));
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "index.html"));
-});
 app.listen(process.env.PORT || 8080);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join("build", "index.html"));
+  });
+}

ad21ece251e1afbefd21be0f735c1bbba4505240
Kevin Bryan Buenaseda <kevinbryanbuenaseda@gmail.com>
2021-11-16 01:32:35 +0800

    1:32am 16/11/21

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join("build", "index.html"));
   });
 }
+
+app.get("/*", (req, res) => {
+  let url = path.join(__dirname, "../client/build", "index.html");
+  if (!url.startsWith("/app/"))
+    // we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
